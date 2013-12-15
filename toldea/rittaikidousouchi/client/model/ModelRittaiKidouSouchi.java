6297020ade2a5593ff87cc205d749fb9226a3427
Lex Offerman <lex.offerman@gmail.com>
2013-12-15 19:42:09 +0100

    Added 'hook shooter' model/rendering

--- toldea/rittaikidousouchi/client/model/ModelRittaiKidouSouchi.java
+++ toldea/rittaikidousouchi/client/model/ModelRittaiKidouSouchi.java
@@ -12,16 +12,20 @@ import toldea.rittaikidousouchi.managers.ModelManager;
 public class ModelRittaiKidouSouchi extends ModelBiped {
 	private IModelCustom modelBackpiece;
 	private IModelCustom modelScabbard;
+	private IModelCustom modelHookShooter;
 
 	public ModelRittaiKidouSouchi() {
 		modelBackpiece = AdvancedModelLoader.loadModel(ModelManager.BACK_PIECE);
 		modelScabbard = AdvancedModelLoader.loadModel(ModelManager.SCABBARD);
+		modelHookShooter = AdvancedModelLoader.loadModel(ModelManager.HOOK_SHOOTER);
 	}
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		renderBackPiece();
-		renderScabbard(.3, 5.0);
-		renderScabbard(-.3, -5.0);
+		renderBackPiece(); // Back Piece
+		renderScabbard(.3, 5.0); // Right Scabbard
+		renderScabbard(-.3, -5.0); // Left Scabbard
+		renderHookShooter(.24, 1.0); // Right Hook Shooter
+		renderHookShooter(-.24, -1.0); // Left Hook Shooter
 	}
 
 	public void renderBackPiece() {
@@ -51,6 +55,20 @@ public class ModelRittaiKidouSouchi extends ModelBiped {
 		GL11.glRotated(yRotation, .0, 1.0, .0);
 
 		modelScabbard.renderAll();
+
+		GL11.glPopMatrix();
+	}
+	
+	public void renderHookShooter(double xOffset, double xScale) {
+		GL11.glPushMatrix();
+
+		GL11.glRotated(180.0, .0, .0, 1.0);
+		GL11.glTranslated(xOffset, -.55, .0);
+		GL11.glScaled(.0625, .0625, .0625);
+
+		GL11.glScaled(.55 * xScale, .55, .55);
+
+		modelHookShooter.renderAll();
 
 		GL11.glPopMatrix();
 	}
--- toldea/rittaikidousouchi/managers/ModelManager.java
+++ toldea/rittaikidousouchi/managers/ModelManager.java
@@ -5,4 +5,5 @@ public abstract class ModelManager {
 	
     public static final String BACK_PIECE = MODEL_LOCATION + "backpiece.obj";
     public static final String SCABBARD = MODEL_LOCATION + "scabbard.obj";
+    public static final String HOOK_SHOOTER = MODEL_LOCATION + "hookshooter.obj";
 }
