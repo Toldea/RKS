f68eabfcf474f3cc7448c8a988b9ad46c662cf66
Lex Offerman <lex.offerman@gmail.com>
2013-12-15 13:53:46 +0100

    Added scabbard model + rendering

--- toldea/rittaikidousouchi/client/model/ModelRittaiKidouSouchi.java
+++ toldea/rittaikidousouchi/client/model/ModelRittaiKidouSouchi.java
@@ -10,24 +10,48 @@ import org.lwjgl.opengl.GL11;
 import toldea.rittaikidousouchi.managers.ModelManager;
 
 public class ModelRittaiKidouSouchi extends ModelBiped {
-	private IModelCustom modelRittaiKidouSouchi;
+	private IModelCustom modelBackpiece;
+	private IModelCustom modelScabbard;
 
 	public ModelRittaiKidouSouchi() {
-		modelRittaiKidouSouchi = AdvancedModelLoader.loadModel(ModelManager.BACK_PIECE);
+		modelBackpiece = AdvancedModelLoader.loadModel(ModelManager.BACK_PIECE);
+		modelScabbard = AdvancedModelLoader.loadModel(ModelManager.SCABBARD);
 	}
-	
+
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		renderBackPiece();
+		renderScabbard(.3, 5.0);
+		renderScabbard(-.3, -5.0);
+	}
+
+	public void renderBackPiece() {
 		GL11.glPushMatrix();
-		
+
 		GL11.glRotated(180.0, .0, .0, 1.0);
 		GL11.glTranslated(.0, -.55, .3);
 		GL11.glScaled(.0625, .0625, .0625);
-		
+
 		GL11.glScaled(.55, .55, .55);
 		GL11.glRotated(45.0, 1.0, .0, .0);
-		
-		modelRittaiKidouSouchi.renderAll();
-		
+
+		modelBackpiece.renderAll();
+
+		GL11.glPopMatrix();
+	}
+
+	public void renderScabbard(double xOffset, double yRotation) {
+		GL11.glPushMatrix();
+
+		GL11.glRotated(180.0, .0, .0, 1.0);
+		GL11.glTranslated(xOffset, -.8, .1);
+		GL11.glScaled(.0625, .0625, .0625);
+
+		GL11.glScaled(.55, .55, .55);
+		GL11.glRotated(15.0, 1.0, .0, .0);
+		GL11.glRotated(yRotation, .0, 1.0, .0);
+
+		modelScabbard.renderAll();
+
 		GL11.glPopMatrix();
 	}
 }
--- toldea/rittaikidousouchi/managers/ModelManager.java
+++ toldea/rittaikidousouchi/managers/ModelManager.java
@@ -2,5 +2,7 @@ package toldea.rittaikidousouchi.managers;
 
 public abstract class ModelManager {
 	private static final String MODEL_LOCATION = "/assets/rittaikidousouchi/models/";
+	
     public static final String BACK_PIECE = MODEL_LOCATION + "backpiece.obj";
+    public static final String SCABBARD = MODEL_LOCATION + "scabbard.obj";
 }
