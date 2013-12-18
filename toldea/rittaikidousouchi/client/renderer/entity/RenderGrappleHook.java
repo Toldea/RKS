1b89788f6d0832b4e61617024d6a7d6825842fdb
Lex Offerman <lex.offerman@gmail.com>
2013-12-18 21:21:44 +0100

    Working on rope rendering

--- toldea/rittaikidousouchi/client/renderer/entity/RenderGrappleHook.java
+++ toldea/rittaikidousouchi/client/renderer/entity/RenderGrappleHook.java
@@ -15,192 +15,125 @@ import toldea.rittaikidousouchi.entity.projectile.EntityGrappleHook.Side;
 public class RenderGrappleHook extends Render {
 
 	private static final ResourceLocation textureGrappleHook = new ResourceLocation("textures/particle/particles.png");
-	
-	public void doRenderGrappleHook(EntityGrappleHook entityGrappleHook, double par2, double par4, double par6, float par8, float par9)
-    {
+
+	public void doRenderGrappleHook(EntityGrappleHook entityGrappleHook, double par2, double par4, double par6, float par8, float par9) {
 		/*
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float)par2, (float)par4, (float)par6);
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glScalef(0.5F, 0.5F, 0.5F);
-        this.bindEntityTexture(entityGrappleHook);
-        */
-        Tessellator tessellator = Tessellator.instance;
-        /*
-        byte b0 = 1;
-        byte b1 = 2;
-        float f2 = (float)(b0 * 8 + 0) / 128.0F;
-        float f3 = (float)(b0 * 8 + 8) / 128.0F;
-        float f4 = (float)(b1 * 8 + 0) / 128.0F;
-        float f5 = (float)(b1 * 8 + 8) / 128.0F;
-        float f6 = 1.0F;
-        float f7 = 0.5F;
-        float f8 = 0.5F;
-        GL11.glRotatef(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0.0F, 1.0F, 0.0F);
-        tessellator.addVertexWithUV((double)(0.0F - f7), (double)(0.0F - f8), 0.0D, (double)f2, (double)f5);
-        tessellator.addVertexWithUV((double)(f6 - f7), (double)(0.0F - f8), 0.0D, (double)f3, (double)f5);
-        tessellator.addVertexWithUV((double)(f6 - f7), (double)(1.0F - f8), 0.0D, (double)f3, (double)f4);
-        tessellator.addVertexWithUV((double)(0.0F - f7), (double)(1.0F - f8), 0.0D, (double)f2, (double)f4);
-        tessellator.draw();
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-        GL11.glPopMatrix();
-         */
-       EntityPlayer ownerPlayer = entityGrappleHook.getOwner();
-        if (ownerPlayer != null)
-        {
-        	/*
-            float f9 = ownerPlayer.getSwingProgress(par9);
-            float f10 = MathHelper.sin(MathHelper.sqrt_float(f9) * (float)Math.PI);
-            Vec3 vec3 = entityGrappleHook.worldObj.getWorldVec3Pool().getVecFromPool(-0.5D, 0.03D, 0.8D);
-            vec3.rotateAroundX(-(ownerPlayer.prevRotationPitch + (ownerPlayer.rotationPitch - ownerPlayer.prevRotationPitch) * par9) * (float)Math.PI / 180.0F);
-            vec3.rotateAroundY(-(ownerPlayer.prevRotationYaw + (ownerPlayer.rotationYaw - ownerPlayer.prevRotationYaw) * par9) * (float)Math.PI / 180.0F);
-            vec3.rotateAroundY(f10 * 0.5F);
-            vec3.rotateAroundX(-f10 * 0.7F);
-            double d3 = ownerPlayer.prevPosX + (ownerPlayer.posX - ownerPlayer.prevPosX) * (double)par9 + vec3.xCoord;
-            double d4 = ownerPlayer.prevPosY + (ownerPlayer.posY - ownerPlayer.prevPosY) * (double)par9 + vec3.yCoord;
-            double d5 = ownerPlayer.prevPosZ + (ownerPlayer.posZ - ownerPlayer.prevPosZ) * (double)par9 + vec3.zCoord;
-            double d6 = ownerPlayer == Minecraft.getMinecraft().thePlayer ? 0.0D : (double)ownerPlayer.getEyeHeight();
+		 * GL11.glPushMatrix(); GL11.glTranslatef((float)par2, (float)par4, (float)par6); GL11.glEnable(GL12.GL_RESCALE_NORMAL); GL11.glScalef(0.5F, 0.5F,
+		 * 0.5F); this.bindEntityTexture(entityGrappleHook);
+		 */
+		Tessellator tessellator = Tessellator.instance;
+		/*
+		 * byte b0 = 1; byte b1 = 2; float f2 = (float)(b0 * 8 + 0) / 128.0F; float f3 = (float)(b0 * 8 + 8) / 128.0F; float f4 = (float)(b1 * 8 + 0) / 128.0F;
+		 * float f5 = (float)(b1 * 8 + 8) / 128.0F; float f6 = 1.0F; float f7 = 0.5F; float f8 = 0.5F; GL11.glRotatef(180.0F - this.renderManager.playerViewY,
+		 * 0.0F, 1.0F, 0.0F); GL11.glRotatef(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F); tessellator.startDrawingQuads(); tessellator.setNormal(0.0F,
+		 * 1.0F, 0.0F); tessellator.addVertexWithUV((double)(0.0F - f7), (double)(0.0F - f8), 0.0D, (double)f2, (double)f5);
+		 * tessellator.addVertexWithUV((double)(f6 - f7), (double)(0.0F - f8), 0.0D, (double)f3, (double)f5); tessellator.addVertexWithUV((double)(f6 - f7),
+		 * (double)(1.0F - f8), 0.0D, (double)f3, (double)f4); tessellator.addVertexWithUV((double)(0.0F - f7), (double)(1.0F - f8), 0.0D, (double)f2,
+		 * (double)f4); tessellator.draw(); GL11.glDisable(GL12.GL_RESCALE_NORMAL); GL11.glPopMatrix();
+		 */
+		EntityPlayer ownerPlayer = entityGrappleHook.getOwner();
+		if (ownerPlayer != null) {
+			/*
+			 * float f9 = ownerPlayer.getSwingProgress(par9); float f10 = MathHelper.sin(MathHelper.sqrt_float(f9) * (float)Math.PI); Vec3 vec3 =
+			 * entityGrappleHook.worldObj.getWorldVec3Pool().getVecFromPool(-0.5D, 0.03D, 0.8D); vec3.rotateAroundX(-(ownerPlayer.prevRotationPitch +
+			 * (ownerPlayer.rotationPitch - ownerPlayer.prevRotationPitch) * par9) * (float)Math.PI / 180.0F); vec3.rotateAroundY(-(ownerPlayer.prevRotationYaw
+			 * + (ownerPlayer.rotationYaw - ownerPlayer.prevRotationYaw) * par9) * (float)Math.PI / 180.0F); vec3.rotateAroundY(f10 * 0.5F);
+			 * vec3.rotateAroundX(-f10 * 0.7F); double d3 = ownerPlayer.prevPosX + (ownerPlayer.posX - ownerPlayer.prevPosX) * (double)par9 + vec3.xCoord;
+			 * double d4 = ownerPlayer.prevPosY + (ownerPlayer.posY - ownerPlayer.prevPosY) * (double)par9 + vec3.yCoord; double d5 = ownerPlayer.prevPosZ +
+			 * (ownerPlayer.posZ - ownerPlayer.prevPosZ) * (double)par9 + vec3.zCoord; double d6 = ownerPlayer == Minecraft.getMinecraft().thePlayer ? 0.0D :
+			 * (double)ownerPlayer.getEyeHeight();
+			 * 
+			 * if (this.renderManager.options.thirdPersonView > 0 || ownerPlayer != Minecraft.getMinecraft().thePlayer) { float f11 =
+			 * (ownerPlayer.prevRenderYawOffset + (ownerPlayer.renderYawOffset - ownerPlayer.prevRenderYawOffset) * par9) * (float)Math.PI / 180.0F; double d7 =
+			 * (double)MathHelper.sin(f11); double d8 = (double)MathHelper.cos(f11); d3 = ownerPlayer.prevPosX + (ownerPlayer.posX - ownerPlayer.prevPosX) *
+			 * (double)par9 - d8 * 0.35D - d7 * 0.85D; d4 = ownerPlayer.prevPosY + d6 + (ownerPlayer.posY - ownerPlayer.prevPosY) * (double)par9 - 0.45D; d5 =
+			 * ownerPlayer.prevPosZ + (ownerPlayer.posZ - ownerPlayer.prevPosZ) * (double)par9 - d7 * 0.35D + d8 * 0.85D; }
+			 * 
+			 * double d9 = entityGrappleHook.prevPosX + (entityGrappleHook.posX - entityGrappleHook.prevPosX) * (double)par9; double d10 =
+			 * entityGrappleHook.prevPosY + (entityGrappleHook.posY - entityGrappleHook.prevPosY) * (double)par9 + 0.25D; double d11 =
+			 * entityGrappleHook.prevPosZ + (entityGrappleHook.posZ - entityGrappleHook.prevPosZ) * (double)par9; double d12 = (double)((float)(d3 - d9));
+			 * double d13 = (double)((float)(d4 - d10)); double d14 = (double)((float)(d5 - d11));
+			 */
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			tessellator.startDrawing(3);
+			tessellator.setColorOpaque_I(0);
+			/*
+			 * byte b2 = 16; for (int i = 0; i <= b2; ++i) { float f12 = (float)i / (float)b2; tessellator.addVertex(par2 + d12 * (double)f12, par4 + d13 *
+			 * (double)(f12 * f12 + f12) * 0.5D + 0.25D, par6 + d14 * (double)f12); System.out.println("Correct Vector: " + (par2 + d12 * (double)f12) + ", " +
+			 * (par4 + d13 * (double)(f12 * f12 + f12) * 0.5D + 0.25D) + ", " + (par6 + d14 * (double)f12) + ")"); }
+			 */
 
-            if (this.renderManager.options.thirdPersonView > 0 || ownerPlayer != Minecraft.getMinecraft().thePlayer)
-            {
-                float f11 = (ownerPlayer.prevRenderYawOffset + (ownerPlayer.renderYawOffset - ownerPlayer.prevRenderYawOffset) * par9) * (float)Math.PI / 180.0F;
-                double d7 = (double)MathHelper.sin(f11);
-                double d8 = (double)MathHelper.cos(f11);
-                d3 = ownerPlayer.prevPosX + (ownerPlayer.posX - ownerPlayer.prevPosX) * (double)par9 - d8 * 0.35D - d7 * 0.85D;
-                d4 = ownerPlayer.prevPosY + d6 + (ownerPlayer.posY - ownerPlayer.prevPosY) * (double)par9 - 0.45D;
-                d5 = ownerPlayer.prevPosZ + (ownerPlayer.posZ - ownerPlayer.prevPosZ) * (double)par9 - d7 * 0.35D + d8 * 0.85D;
-            }
+			tessellator.addVertex(entityGrappleHook.posX - ownerPlayer.posX, entityGrappleHook.posY - ownerPlayer.posY, entityGrappleHook.posZ
+					- ownerPlayer.posZ);
 
-            double d9 = entityGrappleHook.prevPosX + (entityGrappleHook.posX - entityGrappleHook.prevPosX) * (double)par9;
-            double d10 = entityGrappleHook.prevPosY + (entityGrappleHook.posY - entityGrappleHook.prevPosY) * (double)par9 + 0.25D;
-            double d11 = entityGrappleHook.prevPosZ + (entityGrappleHook.posZ - entityGrappleHook.prevPosZ) * (double)par9;
-            double d12 = (double)((float)(d3 - d9));
-            double d13 = (double)((float)(d4 - d10));
-            double d14 = (double)((float)(d5 - d11));
-            */
-            GL11.glDisable(GL11.GL_TEXTURE_2D);
-            GL11.glDisable(GL11.GL_LIGHTING);
-            tessellator.startDrawing(3);
-            tessellator.setColorOpaque_I(0);
-            /*
-			byte b2 = 16;
-            for (int i = 0; i <= b2; ++i)
-            {
-                float f12 = (float)i / (float)b2;
-                tessellator.addVertex(par2 + d12 * (double)f12, par4 + d13 * (double)(f12 * f12 + f12) * 0.5D + 0.25D, par6 + d14 * (double)f12);
-                System.out.println("Correct Vector: " + (par2 + d12 * (double)f12) + ", " + (par4 + d13 * (double)(f12 * f12 + f12) * 0.5D + 0.25D) + ", " + (par6 + d14 * (double)f12) + ")");
-            }
-            */
-            
-            tessellator.addVertex(entityGrappleHook.posX - ownerPlayer.posX, entityGrappleHook.posY - ownerPlayer.posY, entityGrappleHook.posZ - ownerPlayer.posZ);
-            
-            Side side = entityGrappleHook.getSide();
-            
-            if (this.renderManager.options.thirdPersonView > 0 || ownerPlayer != Minecraft.getMinecraft().thePlayer) {
-            	tessellator.addVertex((side == Side.Left ? -.24 : .24), -.55, 0d);
-            } else {
-            	tessellator.addVertex(0d, 0d, 0d);
-            }
+			Side side = entityGrappleHook.getSide();
 
-            tessellator.draw();
-            GL11.glEnable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_TEXTURE_2D);
-        }
-    }
-	
-	protected void kuroiYami(EntityGrappleHook entityGrappleHook, double par2, double par4, double par6, float par8, float par9)
-    {
-        EntityPlayer ownerPlayer = entityGrappleHook.getOwner();
+			if (this.renderManager.options.thirdPersonView > 0 || ownerPlayer != Minecraft.getMinecraft().thePlayer) {
+				tessellator.addVertex((side == Side.Left ? -.24 : .24), -.55, 0d);
+			} else {
+				tessellator.addVertex(0d, 0d, 0d);
+			}
 
-        if (entityGrappleHook != null)
-        {
-            par4 -= (1.6D - (double)ownerPlayer.height) * 0.5D;
-            Tessellator tessellator = Tessellator.instance;
-            double d3 = this.func_110828_a((double)entityGrappleHook.prevRotationYaw, (double)entityGrappleHook.rotationYaw, (double)(par9 * 0.5F)) * 0.01745329238474369D;
-            double d4 = this.func_110828_a((double)entityGrappleHook.prevRotationPitch, (double)entityGrappleHook.rotationPitch, (double)(par9 * 0.5F)) * 0.01745329238474369D;
-            double d5 = Math.cos(d3);
-            double d6 = Math.sin(d3);
-            double d7 = Math.sin(d4);
-            /*
-            if (entity instanceof EntityHanging)
-            {
-                d5 = 0.0D;
-                d6 = 0.0D;
-                d7 = -1.0D;
-            }
-			*/
-            double d8 = Math.cos(d4);
-            double d9 = this.func_110828_a(entityGrappleHook.prevPosX, entityGrappleHook.posX, (double)par9) - d5 * 0.7D - d6 * 0.5D * d8;
-            double d10 = this.func_110828_a(entityGrappleHook.prevPosY + (double)entityGrappleHook.getEyeHeight() * 0.7D, entityGrappleHook.posY + (double)entityGrappleHook.getEyeHeight() * 0.7D, (double)par9) - d7 * 0.5D - 0.25D;
-            double d11 = this.func_110828_a(entityGrappleHook.prevPosZ, entityGrappleHook.posZ, (double)par9) - d6 * 0.7D + d5 * 0.5D * d8;
-            double d12 = this.func_110828_a((double)ownerPlayer.prevRenderYawOffset, (double)ownerPlayer.renderYawOffset, (double)par9) * 0.01745329238474369D + (Math.PI / 2D);
-            d5 = Math.cos(d12) * (double)ownerPlayer.width * 0.4D;
-            d6 = Math.sin(d12) * (double)ownerPlayer.width * 0.4D;
-            double d13 = this.func_110828_a(ownerPlayer.prevPosX, ownerPlayer.posX, (double)par9) + d5;
-            double d14 = this.func_110828_a(ownerPlayer.prevPosY, ownerPlayer.posY, (double)par9);
-            double d15 = this.func_110828_a(ownerPlayer.prevPosZ, ownerPlayer.posZ, (double)par9) + d6;
-            par2 += d5;
-            par6 += d6;
-            double d16 = (double)((float)(d9 - d13));
-            double d17 = (double)((float)(d10 - d14));
-            double d18 = (double)((float)(d11 - d15));
-            GL11.glDisable(GL11.GL_TEXTURE_2D);
-            GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glDisable(GL11.GL_CULL_FACE);
-            boolean flag = true;
-            double d19 = 0.025D;
-            tessellator.startDrawing(5);
-            int i;
-            float f2;
+			tessellator.draw();
+			GL11.glEnable(GL11.GL_LIGHTING);
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+		}
+	}
 
-            for (i = 0; i <= 24; ++i)
-            {
-                if (i % 2 == 0)
-                {
-                    tessellator.setColorRGBA_F(0.5F, 0.4F, 0.3F, 1.0F);
-                }
-                else
-                {
-                    tessellator.setColorRGBA_F(0.35F, 0.28F, 0.21000001F, 1.0F);
-                }
+	protected void kuroiYami(EntityGrappleHook entityGrappleHook, double par2, double par4, double par6, float par8, float par9) {
+		// EntityPlayer ownerPlayer = entityGrappleHook.getOwner();
 
-                f2 = (float)i / 24.0F;
-                tessellator.addVertex(par2 + d16 * (double)f2 + 0.0D, par4 + d17 * (double)(f2 * f2 + f2) * 0.5D + (double)((24.0F - (float)i) / 18.0F + 0.125F), par6 + d18 * (double)f2);
-                tessellator.addVertex(par2 + d16 * (double)f2 + 0.025D, par4 + d17 * (double)(f2 * f2 + f2) * 0.5D + (double)((24.0F - (float)i) / 18.0F + 0.125F) + 0.025D, par6 + d18 * (double)f2);
-            }
+		if (entityGrappleHook != null) {
+			Tessellator tessellator = Tessellator.instance;
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			tessellator.startDrawing(5);
+			int i;
+			float f2;
 
-            tessellator.draw();
-            tessellator.startDrawing(5);
+			for (i = 0; i <= 24; ++i) {
+				if (i % 2 == 0) {
+					tessellator.setColorRGBA_F(0.5F, 0.4F, 0.3F, 1.0F);
+				} else {
+					tessellator.setColorRGBA_F(0.35F, 0.28F, 0.21000001F, 1.0F);
+				}
 
-            for (i = 0; i <= 24; ++i)
-            {
-                if (i % 2 == 0)
-                {
-                    tessellator.setColorRGBA_F(0.5F, 0.4F, 0.3F, 1.0F);
-                }
-                else
-                {
-                    tessellator.setColorRGBA_F(0.35F, 0.28F, 0.21000001F, 1.0F);
-                }
+				f2 = (float) i / 24.0F;
+				tessellator.addVertex(par2 * (double) f2 + 0.0D, par4 * (double) (f2 * f2 + f2) * 0.5D + (double) ((24.0F - (float) i) / 18.0F + 0.125F), par6
+						* (double) f2);
+				tessellator.addVertex(par2 * (double) f2 + 0.025D, par4 * (double) (f2 * f2 + f2) * 0.5D + (double) ((24.0F - (float) i) / 18.0F + 0.125F)
+						+ 0.025D, par6 * (double) f2);
+			}
 
-                f2 = (float)i / 24.0F;
-                tessellator.addVertex(par2 + d16 * (double)f2 + 0.0D, par4 + d17 * (double)(f2 * f2 + f2) * 0.5D + (double)((24.0F - (float)i) / 18.0F + 0.125F) + 0.025D, par6 + d18 * (double)f2);
-                tessellator.addVertex(par2 + d16 * (double)f2 + 0.025D, par4 + d17 * (double)(f2 * f2 + f2) * 0.5D + (double)((24.0F - (float)i) / 18.0F + 0.125F), par6 + d18 * (double)f2 + 0.025D);
-            }
+			tessellator.draw();
+			tessellator.startDrawing(5);
 
-            tessellator.draw();
-            GL11.glEnable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_TEXTURE_2D);
-            GL11.glEnable(GL11.GL_CULL_FACE);
-        }
-    }
-	private double func_110828_a(double par1, double par3, double par5)
-    {
-        return par1 + (par3 - par1) * par5;
-    }
+			for (i = 0; i <= 24; ++i) {
+				if (i % 2 == 0) {
+					tessellator.setColorRGBA_F(0.5F, 0.4F, 0.3F, 1.0F);
+				} else {
+					tessellator.setColorRGBA_F(0.35F, 0.28F, 0.21000001F, 1.0F);
+				}
+
+				f2 = (float) i / 24.0F;
+				tessellator.addVertex(par2 * (double) f2 + 0.0D, par4 * (double) (f2 * f2 + f2) * 0.5D + (double) ((24.0F - (float) i) / 18.0F + 0.125F)
+						+ 0.025D, par6 * (double) f2);
+				tessellator.addVertex(par2 * (double) f2 + 0.025D, par4 * (double) (f2 * f2 + f2) * 0.5D + (double) ((24.0F - (float) i) / 18.0F + 0.125F),
+						par6 * (double) f2 + 0.025D);
+			}
+
+			tessellator.draw();
+			GL11.glEnable(GL11.GL_LIGHTING);
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			GL11.glEnable(GL11.GL_CULL_FACE);
+		}
+	}
+
+	private double func_110828_a(double par1, double par3, double par5) {
+		return par1 + (par3 - par1) * par5;
+	}
 
 	@Override
 	public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9) {
